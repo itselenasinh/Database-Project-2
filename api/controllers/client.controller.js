682152da14116bf201bc2922f1db1bd363358bd6
tfomyuk <115365696+tfomyuk@users.noreply.github.com>
2023-02-28 21:44:53 +0100

    Client READY

--- api/controllers/client.controller.js
+++ api/controllers/client.controller.js
@@ -37,8 +37,26 @@ async function updateClient(req, res) {
 	}
 }
 
+async function deleteClient(req, res) {
+	try {
+		const client = await Client.destroy({
+			where: {
+				id: req.params.id,
+			},
+		})
+		if (client) {
+			return res.status(200).json('Client deleted')
+		} else {
+			return res.status(404).send('Client not found')
+		}
+	} catch (error) {
+		return res.status(500).send(error.message)
+	}
+}
+
 module.exports = {
     getAllClients,
     createClient,
-    updateClient
+    updateClient,
+    deleteClient
   }
--- api/routes/client.router.js
+++ api/routes/client.router.js
@@ -2,10 +2,14 @@ const router = require('express').Router()
 
 const {
   getAllClients,
-  createClient
+  createClient,
+  updateClient,
+  deleteClient
 } = require('../controllers/client.controller')
 
 router.get('/', getAllClients)
 router.post('/', createClient)
+router.put( '/:id', updateClient)
+router.delete( '/:id', deleteClient)
 
 module.exports = router
