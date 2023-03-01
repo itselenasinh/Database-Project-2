55ac380e2cad7915de5ea0342ead3aa55d295e38
itselenasinh <itselenagm@gmail.com>
2023-03-01 19:19:01 +0100

    AM controller done

---
+++ api/controllers/accountManager.controller.js
@@ -0,0 +1,62 @@
+const AccountManager = require('../models/accountManager.model')
+
+async function getAllAccountManagers(req, res) {
+  try {
+    console.log(req.headers)
+    const accountManagers = await AccountManager.findAll()
+    return !accountManagers ? res.status(404).send('No account managers found') : res.status(200).json(accountManagers)
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+async function createAccountManager(req, res) {
+    try {
+      const accountManager = await AccountManager.create(req.body)
+      return res.status(200).json({ message: 'Account Manager created', accountManager: accountManager })
+    } catch (error) {
+      return res.status(500).send(error.message)
+    }
+  }
+
+async function updateAccountManager(req, res) {
+	try {
+		const [accountManagerExist, accountManager] = await AccountManager.update(req.body, {
+			returning: true,
+			where: {
+				id: req.params.id,
+			},
+		})
+        if (accountManagerExist !== 0) {
+			return res.status(200).json({ message: 'Account Manager updated', accountManager: accountManager })
+		} else {
+			return res.status(404).send('Account Manager not found')
+		}
+	} catch (error) {
+		return res.status(500).send(error.message)
+	}
+}
+
+async function deleteAccountManager(req, res) {
+	try {
+		const accountManager = await AccountManager.destroy({
+			where: {
+				id: req.params.id,
+			},
+		})
+		if (accountManager) {
+			return res.status(200).json('Account Manager deleted')
+		} else {
+			return res.status(404).send('Account Manager not found')
+		}
+	} catch (error) {
+		return res.status(500).send(error.message)
+	}
+}
+
+module.exports = {
+    getAllAccountManagers,
+    createAccountManager,
+    updateAccountManager,
+    deleteAccountManager
+  }
